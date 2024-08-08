a4dc3021dd90b13c03f39790756829f9e4573b57
Sophia Yeung <yhwhiuwai@gmail.com>
2024-08-08 22:44:00 +0800

    Add files via upload

---
+++ unlimited encouragements.py
@@ -0,0 +1,7 @@
+question = input('Can you find an internship? Enter "y" if yes, "n" if no: ')
+while True:
+    if question == 'y':
+        print('I told you can!')
+        break
+    else:
+        print("Don't give up! I believe you can") #unlimited encouragements
